4e71c5cb7c253335086feb1cfe415ca0d9d3c801
bizliaoyuan <bizliaoyuan@gmail.com>
2024-05-09 01:44:12 +0800

    Update NodeReward.sol

--- contracts/NodeReward.sol
+++ contracts/NodeReward.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 // import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import "./EIP712Paymaster.sol";
